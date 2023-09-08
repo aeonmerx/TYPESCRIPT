7eeebac6f2ade8edc2bcd9393bcdc2d4475c3080
aeonmerx <103615875+aeonmerx@users.noreply.github.com>
2023-09-08 15:04:35 -0500

    Update calculadora.ts

--- extras/calculadora.ts
+++ extras/calculadora.ts
@@ -4,7 +4,6 @@ const calculadora = (a: number, b: number, operador: string) =>
   operador === "*" ? a * b :
   operador === "/" ? (b === 0 ? "División por cero no permitida" : a / b) :
   "Operador no válido";
-
 // Ejemplo de uso
 const resultadoSuma = calculadora(5, 3, "+");
 console.log("Resultado de la suma:", resultadoSuma); // 8
